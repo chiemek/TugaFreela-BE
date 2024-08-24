2edd71dbe25d25b8ce3d8114d877c220b46637f0
chiemek <ekeson1085@gmail.com>
2024-08-24 13:11:02 +0100

    corrected my login

--- app.js
+++ app.js
@@ -748,7 +748,7 @@ app.post("/login", async (req, res) => {
   }
 
   try {
-    const user = await User.findOne({ email });
+    const user = await user.findOne({ email });
     if (!user) {
       return res.status(401).json({ error: "Invalid email" });
     }
